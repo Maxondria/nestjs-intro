58819abf8b3600f40ba89b60c880bb77f63c67b7
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-08-30 00:24:52 +0300

    Connect to MongoAtlas and create product schema

--- src/products/product.model.ts
+++ src/products/product.model.ts
@@ -1,3 +1,10 @@
+import * as mongoose from 'mongoose';
+
+export const ProductSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+});
 export class Product {
   constructor(
     public title: string,
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { ProductSchema } from './product.model';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }]),
+  ],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
